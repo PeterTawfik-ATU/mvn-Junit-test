add27a6564580aecb728bb798f7d027671e65279
Peter <p.magdytab@gmail.com>
2023-10-22 08:24:52 +0200

    mavan-Junit5-peter-test

---
+++ mvnTest1/mvnApp-peter/src/test/java/ie/atu/MvnAppspeter/AppTest.java
@@ -0,0 +1,20 @@
+package ie.atu.MvnAppspeter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App peter L00186456@atu.ie
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
